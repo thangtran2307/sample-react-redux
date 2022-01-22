fdbb04c551183d5af10dfe6332ac425bf29966e8
Thang Tran <thangtran2307@gmail.com>
2022-01-22 13:02:22 +0800

    - Added packages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <div>
       <h1>Sample React Redux</h1>
+      <h2>
+        Backend Endpoint:
+        {' '}
+        {process.env.REACT_APP_BACKEND_ENDPOINT}
+      </h2>
     </div>
   );
 }
